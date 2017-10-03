fffe9020290ab883a21b281f0aa1b4f400048337
purplesunset871019 <hsb41202@gmail.com>
2017-10-03 16:34:29 +0800

    first commit

---
+++ src/com/tom/Person.java
@@ -0,0 +1,8 @@
+package com.tom;
+
+public class Person {
+	public void sayHello(){
+	System.out.println("Hello!");
+	}
+	
+}
---
+++ src/myproject/Hello.java
@@ -0,0 +1,14 @@
+package myproject;
+
+import com.tom.Person;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		Person p = new Person();
+		p.sayHello();
+	}
+
+}
