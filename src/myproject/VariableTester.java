81e86129bc8ac2efffacbd0daaaee44eab888fd4
purplesunset871019 <hsb41202@gmail.com>
2017-10-24 16:36:13 +0800

    1234

--- src/myproject/VariableTester.java
+++ src/myproject/VariableTester.java
@@ -1,40 +1,18 @@
 package myproject;
 
-import java.util.Random;
+import java.util.*;
 
 public class VariableTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    
-		int age = 87 ;
-        int b;
-        int c;
-        int d;
-        b=age*2;
-        d=b*b;
-        c=d*age;
-        age=c;
-        
+   
         short e = 6 ;
         float f = 15.6f ;
         char g = 65 ;
         char g2 = 'Z';
         int h=g2;
         boolean pass = true;
-    
-    
-    System.out.println(age); 
-    System.out.println(b); 
-    System.out.println(c); 
-    System.out.println(d); 
-    System.out.println(e); 
-    System.out.println(f); 
-    System.out.println(g); 
-    System.out.println(g2); 
-    System.out.println(h); 
-    System.out.println(pass); 
-    System.out.println(!pass);
     
     boolean pass2 = pass;
     System.out.println(pass2);
@@ -43,6 +21,44 @@ public class VariableTester {
     String s = new String("Hellow"); 
     Random m = new Random();
     int n1 = m.nextInt();
+    int len = s.length();
+    System.out.println("len," + len); 
+    System.out.println(s.charAt(0)); 
+
+    System.out.println("player1 please enter your name");     
+    Scanner scanner = new Scanner(System.in);
+    String line1 = scanner.nextLine();
+    System.out.println("Hello,"+line1);
+	
+    System.out.println("Which number do you choose");
+    line1 = scanner.nextLine();
+    int number1 = Integer.parseInt(line1);
+    
+    System.out.println("player2 please enter your name");
+    Scanner scanner2 = new Scanner(System.in);
+    String line2 = scanner2.nextLine();
+    System.out.println("Hello,"+line2); 
+    
+    
+    player2=line2;
+    
+    System.out.println("Which number do you choose?");
+    line2 = scanner2.nextLine();
+    int number2 = Integer.parseInt(line2);
+        
+    if(number1 > number2)
+        {
+	    System.out.println(line1 +"win");
+        }
+  
+    else if(number1 < number2)
+        {
+	    System.out.println(line2 +"win");
+        }
+    else
+        {
+	    System.out.println("Draw");
+        }
     
 	}
 
