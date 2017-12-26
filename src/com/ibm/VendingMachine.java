da52fb6ff4c0809820d2423025b61b329c20bdb3
purplesunset871019 <hsb41202@gmail.com>
2017-12-26 16:30:58 +0800

    2017/12/26

--- src/com/ibm/VendingMachine.java
+++ src/com/ibm/VendingMachine.java
@@ -8,26 +8,131 @@ public class VendingMachine {
 		// TODO Auto-generated method stub
 
 		int total=0;
-		int money;
+		String money;
 		int a;
-		
-		System.out.println("販賣機餘額"+total);     
-		System.out.print("請投幣或選擇飲料(a,b,c),或輸入0結束"+":");		
+		for(a=1;;a++)
+		{
+			System.out.println("販賣機餘額"+total);     
+			System.out.print("請投幣或選擇飲料(a,b,c),或輸入0結束"+":");		
 
-		Scanner scanner = new Scanner(System.in);
-	    String number1 = scanner.nextLine();
-	    money = Integer.parseInt(number1);
-		
-	    switch(money)
-	    {
-	    case 1:	    
-	    	
-	    case 5:
-	    	
-	    case 10:
-	    	
-	    case "a":
-	    }
+			Scanner scanner = new Scanner(System.in);
+		    String Price = scanner.nextLine();
+
+			/*if(Price=="1"||Price=="5"||Price=="10"||Price=="a"||Price=="b"||Price=="c")
+			{
+				switch(Price)
+			    {
+			    case "1":
+			    	total=total+1;
+			    	break;
+			    case "5":
+			    	total=total+5;
+			    	break;
+			    case "10":
+			    	total=total+10;
+			    	break;
+			    case "a":
+			    	if(total>=15)
+			    	{
+				    	total=total-15;
+				    	System.out.println("DON!");
+			    	}
+			    	
+			    	else
+			    	{
+				    	System.out.println("BEEP!");
+			    	}
+			    	break;
+			    case "b":
+			    	if(total>=20)
+			    	{
+				    	total=total-20;
+				    	System.out.println("DON!");
+			    	}
+			    	
+			    	else
+			    	{
+				    	System.out.println("BEEP!");
+			    	}
+			    	break;
+			    case "c":
+			    	if(total>=30)
+			    	{
+				    	total=total-30;
+				    	System.out.println("DON!");
+			    	}
+			    	
+			    	else
+			    	{
+				    	System.out.println("BEEP!");
+			    	}
+			    	break;
+
+			    }
+
+		    }
+			else if(Price=="0")
+			{
+				break;
+			}*/
+		    
+		    if(Price=="1")
+		    {
+		    	total=total+1;
+		    }
+		    else if(Price=="5")
+		    {
+		    	total=total+5;
+		    }
+		    else if(Price=="10")
+		    {
+		    	total=total+10;
+		    }
+		    else if(Price=="a")
+		    {
+		    	if(total>=15)
+		    	{
+			    	total=total-15;
+			    	System.out.println("DON!");
+		    	}
+		    	
+		    	else
+		    	{
+			    	System.out.println("BEEP!");
+		    	}
+		    }
+		    else if(Price=="b")
+		    {
+		    	if(total>=20)
+		    	{
+			    	total=total-20;
+			    	System.out.println("DON!");
+		    	}
+		    	
+		    	else
+		    	{
+			    	System.out.println("BEEP!");
+		    	}
+		    }
+		    else if(Price=="c")
+		    {
+		    	if(total>=30)
+		    	{
+			    	total=total-30;
+			    	System.out.println("DON!");
+		    	}
+		    	
+		    	else
+		    	{
+			    	System.out.println("BEEP!");
+		    	}
+		    }
+		    else if(Price=="0")
+		    {
+		    	break;
+		    }
+		    
+		}
 	}
 
 }
