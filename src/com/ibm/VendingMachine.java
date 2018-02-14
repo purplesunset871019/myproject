1d11c0ff5448418facd8c937d563e8385381722b
“purplesunset871019” <hsb41202@gmail.com>
2018-02-14 23:41:23 +0800

    wintter vacation practice3

--- src/com/ibm/VendingMachine.java
+++ src/com/ibm/VendingMachine.java
@@ -8,22 +8,27 @@ public class VendingMachine {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("a=15");
+		System.out.println("b=20");   
+		System.out.println("c=30");   
+		
 		int total=0;
-		String money;
 		int a;
-		for(a=1;;a++)
+
+		
+		for(a=1;;a++)			
 		{
 			System.out.println("販賣機餘額"+total);     
-			System.out.print("請投幣或選擇飲料(a,b,c),或輸入0結束"+":");		
+			System.out.print("請投幣或選擇飲料(a,b,c),或輸入0結束:");		
 
 			Scanner scanner = new Scanner(System.in);
 		    String Price = scanner.nextLine();
+		    
+			boolean leave = true;
 
-			/*if(Price=="1"||Price=="5"||Price=="10"||Price=="a"||Price=="b"||Price=="c")
-			{
 				switch(Price)
 			    {
-			    case "1":
+				case "1":
 			    	total=total+1;
 			    	break;
 			    case "5":
@@ -68,70 +73,15 @@ public class VendingMachine {
 				    	System.out.println("BEEP!");
 			    	}
 			    	break;
-
+			    case "0":
+			    	leave = false;
+			    	break;
 			    }
 
-		    }
-			else if(Price=="0")
+			if(leave)
 			{
 				break;
-			}*/
-		    
-		    if(Price=="1")
-		    {
-		    	total=total+1;
-		    }
-		    else if(Price=="5")
-		    {
-		    	total=total+5;
-		    }
-		    else if(Price=="10")
-		    {
-		    	total=total+10;
-		    }
-		    else if(Price=="a")
-		    {
-		    	if(total>=15)
-		    	{
-			    	total=total-15;
-			    	System.out.println("DON!");
-		    	}
-		    	
-		    	else
-		    	{
-			    	System.out.println("BEEP!");
-		    	}
-		    }
-		    else if(Price=="b")
-		    {
-		    	if(total>=20)
-		    	{
-			    	total=total-20;
-			    	System.out.println("DON!");
-		    	}
-		    	
-		    	else
-		    	{
-			    	System.out.println("BEEP!");
-		    	}
-		    }
-		    else if(Price=="c")
-		    {
-		    	if(total>=30)
-		    	{
-			    	total=total-30;
-			    	System.out.println("DON!");
-		    	}
-		    	
-		    	else
-		    	{
-			    	System.out.println("BEEP!");
-		    	}
-		    }
-		    else if(Price=="0")
-		    {
-		    	break;
-		    }
+			}
 		    
 		}
 	}
