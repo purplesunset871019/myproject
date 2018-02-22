6cc5036a7382912a6d60ae5820e4fc0572082edb
“purplesunset871019” <hsb41202@gmail.com>
2018-02-22 21:48:54 +0800

    wintter vacation practice5

--- src/myproject/sogo/Statistics.java
+++ src/myproject/sogo/Statistics.java
@@ -3,32 +3,28 @@ package myproject.sogo;
 import java.beans.Customizer;
 import java.util.ArrayList;
 
-public class Tester {
+public class Statistics {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Customer> customers=new ArrayList<>();
+		/*ArrayList<Customer> customers=new ArrayList<>();
 		
 		customers.add(new Customer(10000));
 		customers.add(new SliverCustomer(10000));
 		customers.add(new GoldCustomer(10000));
-		
-		/*int n=list.get(2);
-		int n2=list.get(3);
-		
-		System.out.println(list.get(1));
-		System.out.println(list.size());*/
-		
+				
 		for(int i=0;i<customers.size();i++)
 		{
 			Customer c=customers.get(i);
 			c.print();
-		}
+		}*/
 		
-		Customer c1= new Customer(1000);
+		Customer c1= new Customer(1004);
 		c1.print();
-		SliverCustomer c2=new SliverCustomer(1000);
+		SliverCustomer c2=new SliverCustomer(1004);
 		c2.print();
+	    GoldCustomer c3=new GoldCustomer(1004);
+		c3.print();
 		
 	}
 
