e059b9158a0be8f8c69609d1ae2e86323a14bb38
purplesunset871019 <hsb41202@gmail.com>
2018-05-08 15:12:04 +0800

    2018/5/8 collection1

---
+++ src/com/java2/collection/Main.java
@@ -0,0 +1,19 @@
+package com.java2.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Integer> list = new ArrayList()<>();
+		list.add(2);
+		list.add(0);
+		list.add(1);
+		list.add(7);
+	    System.out.println(list);
+	}
+
+}
